06eed7476055e7ccf7444a1336f43e1ea1e995fc
amerelo <alexis.merelo@gmail.com>
2023-02-22 19:55:28 +0100

    add db acount details

--- car_api/migrations/20221213153434_init_tables.up.sql
+++ car_api/migrations/20221213153434_init_tables.up.sql
@@ -1,7 +1,7 @@
 -- Add up migration script here
 
 CREATE TABLE users (
-	user_id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
 	email VARCHAR UNIQUE NOT NULL,
 	password_hash VARCHAR NOT NULL,
 	created_at timestamptz NOT NULL DEFAULT now(),
@@ -11,21 +11,21 @@ CREATE TABLE users (
 -- car details and bank details
 
 CREATE TABLE car (
-	car_id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-	user_id uuid NOT NULL REFERENCES users (user_id) ON DELETE CASCADE,
-	car_plate VARCHAR NOT NULL,
+	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	user_id uuid NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+	plate VARCHAR NOT NULL,
+	model VARCHAR NOT NULL,
 	created_at timestamptz NOT NULL DEFAULT now(),
 	updated_at TIMESTAMP  NOT NULL DEFAULT current_timestamp
 );
 
--- CREATE INDEX car_user ON car (note_id, user_id);
+CREATE TABLE bank_details (
+	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	user_id uuid NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+	country VARCHAR NOT NULL,
+	iban VARCHAR NOT NULL,
+	acount_name VARCHAR NOT NULL,
+	created_at timestamptz NOT NULL DEFAULT now(),
+	updated_at TIMESTAMP  NOT NULL DEFAULT current_timestamp
+);
 
--- CREATE TABLE bank_details (
--- 	car_id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
--- 	user_id uuid NOT NULL REFERENCES users (user_id) ON DELETE CASCADE,
--- 	car_plate VARCHAR NOT NULL,
--- 	-- region ?
--- 	-- car type ?
--- 	-- created_at timestamptz NOT NULL DEFAULT now(),
--- 	-- updated_at TIMESTAMP  NOT NULL DEFAULT current_timestamp
--- );
